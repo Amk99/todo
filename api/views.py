4648fa2f6282b5349cdb2365d95f7377a9817b0b
atharva kotibhaskar <atharvatamk@gmail.com>
2023-02-15 22:32:22 +0530

    added delete endpoint

--- api/views.py
+++ api/views.py
@@ -5,13 +5,16 @@ from rest_framework import status
 from .serializers import TodoSerializer
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.decorators import api_view
+from django.shortcuts import get_object_or_404
 
-
-
+# View To diplay List of Tasks
 class TaskList(generics.ListAPIView):
         queryset = Todo.objects.all()
         serializer_class = TodoSerializer
 
+
+# Create New Task
 class TaskCreateView(generics.CreateAPIView):
     model = Todo
     serializer_class = TodoSerializer
@@ -19,17 +22,39 @@ class TaskCreateView(generics.CreateAPIView):
         serializer.save()
 
 
+# Mark a task as Completed
 class MarkAsCompleted(APIView):
     def put(self, request, pk):
-        todo = Todo.objects.get(pk=pk)
+        todo = get_object_or_404(Todo ,pk=pk)
         todo.completed = True
         todo.save()
         return Response({'Message': 'Task marked as completed'}, status=status.HTTP_200_OK)
 
 
+# Mark Task as incomplete
 class MarkAsInComplete(APIView):
     def put(self, request, pk):
-        todo = Todo.objects.get(pk=pk)
+        todo = get_object_or_404(Todo, pk=pk)
         todo.completed = True
         todo.save()
         return Response({'Message': 'Task marked as Incomplete'}, status=status.HTTP_200_OK)
+
+
+# Update Task
+class update_todo(generics.UpdateAPIView):
+    serializer_class = TodoSerializer
+    def put(self, request, pk):
+        todo = get_object_or_404(Todo, pk=pk)
+        serializer = TodoSerializer(instance = todo,data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+        return Response(serializer.data)
+
+
+# Delete Task
+class TaskDelete(APIView):
+    def delete(self,request, pk):
+        task = get_object_or_404(Todo,pk = pk)
+        task.delete()
+        return Response("Task deleted successfully.")
+
